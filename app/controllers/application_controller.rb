9680b5f23b79219cdd2901ce0bcc228c16dd885e
Baudin-Laurencin <abaudin@gmail.com>
2017-09-25 10:15:48 +0000

    Modification des vues Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+  # Section 4, session 17 - https://www.udemy.com/apprendre-a-coder-une-plateforme-airbnb/learn/v4/t/lecture/6767878?start=0
   
   
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   
-  protected
+protected
   def configure_permitted_parameters
-    devise_parameter_sanitize.permit(:sign_up, keys: [:fullname])
-    devise_parameter_sanitize.permit(:account_update, keys: [:fullname])
-    
-    
-    
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname])
   end
 end
+
+
