3be0ce3f11b5edf6a74f434a879ae1be5e1c4235
Baudin-Laurencin <abaudin@gmail.com>
2017-09-29 08:58:15 +0000

    correction pour erreur eroku

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class  RegistrationsController < Devise::RegistrationsController
     
     Protected
     def update_resource(resource, params)
-        resource.update_without_password(params  )
+        resource.update_without_password(params)
     
     end
 end
